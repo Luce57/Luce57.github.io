a733a424242028b3d4f986b78b6144abde9db90c
Luce <lucie.dasilva@apprenant.ifapme.be>
2024-05-23 19:46:07 +0200

    main

--- Mes projets/galerieDart/Problemes/oeuvre_pageController.php
+++ Mes projets/galerieDart/Problemes/oeuvre_pageController.php
@@ -1,5 +1,5 @@
 <?php
-require($_SERVER['DOCUMENT_ROOT'] . "/galerieDart/model/euvre_pageModel.php");
+require($_SERVER['DOCUMENT_ROOT'] . "/galerieDart/model/oeuvre_pageModel.php");
 $db = new PDO('mysql:host=localhost;dbname=galerie_art;charset=utf8', 'username', 'password');
 $artModel = new ArtModel($db);
 $artObjects = $artModel->getArtObjects();
